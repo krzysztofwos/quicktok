4a5da3bbd25049156369918c0d6206a631076e40
Krzysztof Woś <krzysztof.wos@pm.me>
2026-01-05 18:35:10 +0900

    Upgrade PyO3 to 0.27
    - Update PyO3 dependency from 0.20.3 to 0.27
    - Migrate to new Bound-based module API
    - Fixes Clippy non_local_definitions error

--- quicktok/src/lib.rs
+++ quicktok/src/lib.rs
@@ -5,7 +5,7 @@ use pyo3::prelude::*;
 pub use crate::tokenizer::{basic::BasicTokenizer, Tokenizer};
 
 #[pymodule]
-fn quicktok(_py: Python, module: &PyModule) -> PyResult<()> {
+fn quicktok(module: &Bound<'_, PyModule>) -> PyResult<()> {
     module.add_class::<BasicTokenizer>()?;
     Ok(())
 }
